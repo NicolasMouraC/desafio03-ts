da68921aedb050010334acd018cc9136d06e8b1e
Nicolas <nicolasoutog@gmail.com>
2023-06-05 17:36:37 -0300

    test

--- src/services/login.test.tsx
+++ src/services/login.test.tsx
@@ -3,13 +3,13 @@ import { login } from "./login"
 describe('login', () => {
 
     const mockEmail = 'nath@dio.bank'
-    it('Deve exibir um alert com boas vindas caso o email seja válido', async() => {
-        const response = await login(mockEmail)
+    it('Deve retornar verdadeiro caso o email e senha sejam válidos', async() => {
+        const response = await login(mockEmail, '123456')
         expect(response).toBeTruthy()
     })
 
-    it('Deve exibir um erro caso o email seja inválido', async() => {
-        const response = await login('email@invalido.com')
+    it('Deve retornar falso caso o email ou senha sejam inválidos', async() => {
+        const response = await login('email@invalido.com', '12123')
         expect(response).toBeFalsy()
     })
 })
